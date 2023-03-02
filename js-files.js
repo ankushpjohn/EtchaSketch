4a2b2a1c4474fb94eab3ab76f03a5118e0748f40
ankushpjohn <ankushjohn22@gmail.com>
2023-03-02 11:24:06 +0530

    Added size slider and formatting

--- js-files.js
+++ js-files.js
@@ -1,23 +1,32 @@
 const grid = document.querySelector('.grid');
 let n = 32;
+createGrid(n);
 
-for(let i = 0; i<n; i++) {
-    row = document.createElement('div');
-    row.classList.add('row');
-    grid.appendChild(row);
-    for(let j = 0; j<n; j++) {
-        let elem = document.createElement('div');
-        elem.classList.add('elem');
+function createGrid(n) {
+    for (let i = 0; i < n; i++) {
+        row = document.createElement('div');
+        row.classList.add('row');
+        grid.appendChild(row);
+        for (let j = 0; j < n; j++) {
+            let elem = document.createElement('div');
+            elem.classList.add('elem');
 
-        elem.addEventListener('mouseover', () => {
-            elem.classList.add('hov');
-        });
-        row.appendChild(elem);
+            elem.addEventListener('mouseover', () => {
+                elem.classList.add('hov');
+            });
+            row.appendChild(elem);
+        }
     }
 }
 
-reset = document.querySelector('.reset');
-reset.addEventListener('click', () => {
-    document.querySelectorAll('.hov').forEach((e) => e.classList.remove('hov'));
-});
+const reset = document.querySelector('.reset');
+reset.addEventListener('click', () => document.querySelectorAll('.hov').forEach((e) => e.classList.remove('hov')));
 
+resize = document.querySelector('input');
+resize.addEventListener('input', (e) => {
+    n = e.target.value;
+    while (grid.hasChildNodes()) {
+        grid.removeChild(grid.firstChild);
+    }
+    createGrid(n);
+});
